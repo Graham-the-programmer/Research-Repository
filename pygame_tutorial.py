dc24fe8fef7cc8da5d0a3a3d6ea4df15ee895fa3
Graham <gblackledge88@gmail.com>
2023-09-14 11:30:23 +1200

    Add spaceships and movement

--- pygame_tutorial.py
+++ pygame_tutorial.py
@@ -8,14 +8,19 @@ pygame.display.set_caption('Tutorial game')
 WHITE = (255, 255, 255)
 
 FPS = 60 
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP =pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90 )
+
 RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
 
-def draw_window():
+def draw_window(red, yellow):
      WIN.fill((WHITE))
-     WIN.blit(YELLOW_SPACESHIP_IMAGE, (300, 100))
+     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+     WIN.blit(RED_SPACESHIP, (red.x, red.y))
      pygame.display.update()
 
 
@@ -23,6 +28,10 @@ def draw_window():
 
 
 def main():
+    red = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
+
     clock = pygame.time.Clock()
     #this code handles being able to exit the game window without using a force quit
     run = True
@@ -31,8 +40,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
-        draw_window()
+        red.x += 1
+        draw_window(red, yellow)
 
     pygame.quit()
 
